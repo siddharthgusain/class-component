cdb35b4cc198da76fc46a4e0d439291a590ebf23
siddharth gusain <siddharthgusain26011999@gmail.com>
2019-03-22 02:15:44 +0530

    Using geolocation and conditional rendering done

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import './SeasonDisplay.css';
+const seasonConfig = {
+    summer:{
+        text:'Lets go to Beach',
+        iconName:'sun'
+    },
+    winter:{
+        text:'It is Chilly',
+        iconName:'snowflake'
+    }
+}
+const getSeason = (lat,month)=>{
 
+    if(month>3 && month<9){
+       return  lat > 0 ? 'summer':'winter';
+    }
+    else{
+        return lat>0 ? 'winter':'summer';
+    }
+}
 
-const SeasonDisplay = ()=>{
+const SeasonDisplay = (props)=>{
+    
+    const season = getSeason(props.lat,new Date().getMonth());
+    const {text,iconName}=seasonConfig[season];
 
-    return <div>Season</div>
+    return (
+    <div className={`season-display ${season}`}>
+        <i className={`icon-left massive icon ${iconName}`} />
+        <h1>{text}</h1>
+        <i className={`icon-right massive icon ${iconName}`} />
+        
+    </div>
+    );
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 class App extends Component{
 
-    constructor(props){
-        super(props);
-        this.state = {lat:null,err:''};
+  state={
+      lat:null,
+      err:''
+    };
 
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
             (pos)=>this.setState({lat:pos.coords.latitude}),
             (err)=>this.setState({err:err.message})
         );
     }
     
+    renderConditionally(){
+
+        if(this.state.err && !this.state.lat){
+            return <div>Error:{this.state.err}</div>;
+       }
+
+       if(!this.state.err && this.state.lat){
+           return <SeasonDisplay lat={this.state.lat}/>;
+       }
+
+       return <Spinner message="Please accept location request"/>
+
+    }
+
     render(){
       
-           if(this.state.err && !this.state.lat){
-                return <div>Error:{this.state.err}</div>
-           }
-
-           if(!this.state.err && this.state.lat){
-               return <div>Latitude:{this.state.lat}</div>
-           }
-
-           return <div>Loading....</div>
-
+          return (
+            <div >
+                {this.renderConditionally()}
+            </div>
+          );
        
     }
 }
